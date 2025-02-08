597851d813af1ce1b0dd51859edede0f87484c0e
tsubasamusu <tsubasa_03_08@icloud.com>
2025-02-08 17:41:16 +0900

    [add] #1 SEnhancedCommentNode クラスを作成（処理は空）

---
+++ Source/AutoCommentGenerator/Public/EnhancedCommentNode.h
@@ -0,0 +1,11 @@
+// Copyright (c) 2025, tsubasamusu All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "SGraphNodeComment.h"
+
+class SEnhancedCommentNode : public SGraphNodeComment
+{
+
+};
