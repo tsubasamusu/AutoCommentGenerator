76c6ed6076f50b3aceab618d60cd221b560c091f
tsubasamusu <tsubasa_03_08@icloud.com>
2025-02-10 17:22:43 +0900

    [add] #1 FPinData 構造体と FNodeData 構造体、FNodesData 構造体を作成

---
+++ Source/AutoCommentGenerator/Public/NodesData.h
@@ -0,0 +1,40 @@
+// Copyright (c) 2025, tsubasamusu All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "NodesData.generated.h"
+
+USTRUCT()
+struct FPinData
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY();
+    FString PinName;
+};
+
+USTRUCT()
+struct FNodeData
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY()
+    TArray<FPinData> Pins;
+};
+
+USTRUCT()
+struct FNodesData
+{
+    GENERATED_BODY()
+
+public:
+    UPROPERTY()
+    TArray<FNodeData> Nodes;
+
+    FNodesData() :Nodes(TArray<FNodeData>()) {}
+
+    FNodesData(const TArray<FNodeData>& InNodesData) :Nodes(InNodesData) {}
+};
