19f045bf943082dee86ee21265c0fe0662082323
tsubasamusu <tsubasa_03_08@icloud.com>
2025-02-22 08:45:21 +0900

    [add] #1 SGptLanguageComboButton クラスと FLocalizedCulturesFlyweight 構造体を作成

---
+++ Source/AutoCommentGenerator/Private/GptLanguageComboButton.cpp
@@ -0,0 +1,83 @@
+// Copyright (c) 2025, tsubasamusu All rights reserved.
+
+#include "GptLanguageComboButton.h"
+#include "Internationalization/Culture.h"
+#include "Widgets/Layout/SBox.h"
+#include "Widgets/Input/SComboButton.h"
+#include "InternationalizationSettingsModel.h"
+#include "SCulturePicker.h"
+#include "DetailCategoryBuilder.h"
+#include "DetailLayoutBuilder.h"
+#include "IDetailCustomization.h"
+#include "DetailWidgetRow.h"
+#include "Widgets/Input/SButton.h"
+#include "Misc/MessageDialog.h"
+#include "Widgets/Input/SEditableTextBox.h"
+#include "UnrealEdGlobals.h"
+#include "Modules/ModuleManager.h"
+
+#define LOCTEXT_NAMESPACE "GptLanguageComboButton"
+
+void SGptLanguageComboButton::Construct(const FArguments& InArgs, const TSharedRef<FLocalizedCulturesFlyweight>& InLocalizedCulturesFlyweight)
+{
+    LocalizedCulturesFlyweight = InLocalizedCulturesFlyweight;
+
+    ChildSlot
+        [
+            SAssignNew(ComboButton, SComboButton)
+                .ButtonContent()
+                [
+                    SNew(STextBlock)
+                        .Text(this, &SGptLanguageComboButton::GetDesiredComboButtonText)
+                ]
+        ];
+
+    ComboButton->SetOnGetMenuContent(FOnGetContent::CreateSP(this, &SGptLanguageComboButton::OnGetComboButtonMenuContent));
+}
+
+FText SGptLanguageComboButton::GetDesiredComboButtonText() const
+{
+    if (SelectedCulture.IsValid()) return FText::FromString(SelectedCulture->GetNativeName());
+
+    FCulturePtr CurrentCulture = FInternationalization::Get().GetCurrentLanguage();
+
+    if (CurrentCulture.IsValid()) return FText::FromString(CurrentCulture->GetNativeName());
+
+    return LOCTEXT("None", "None");
+}
+
+TSharedRef<SWidget> SGptLanguageComboButton::OnGetComboButtonMenuContent()
+{
+ 	FCulturePtr CurrentCulture = FInternationalization::Get().GetCurrentLanguage();
+ 
+ 	const auto& CulturePicker = SNew(SCulturePicker)
+ 		.InitialSelection(CurrentCulture)
+ 		.OnSelectionChanged_Lambda([this](const FCulturePtr& InSelectedCulture, ESelectInfo::Type SelectInfo)
+            {
+				SelectedCulture = InSelectedCulture;
+
+                if (ComboButton.IsValid()) ComboButton->SetIsOpen(false);
+            })
+ 		.IsCulturePickable_Lambda([this](FCulturePtr Culture) -> bool
+            {
+                TArray<FString> CultureNames = Culture->GetPrioritizedParentCultureNames();
+
+                for (const FString& CultureName : CultureNames)
+                {
+                    if (LocalizedCulturesFlyweight->LocalizedCultures.Contains(Culture)) return true;
+                }
+
+                return false;
+            })
+ 		.DisplayNameFormat(SCulturePicker::ECultureDisplayFormat::ActiveAndNativeCultureDisplayName)
+ 		.ViewMode(SCulturePicker::ECulturesViewMode::Flat);
+ 
+ 	return SNew(SBox)
+ 		.MaxDesiredHeight(500.0f)
+ 		.WidthOverride(350.0f)
+ 		[
+ 			CulturePicker
+ 		];
+}
+
+#undef LOCTEXT_NAMESPACE
---
+++ Source/AutoCommentGenerator/Public/GptLanguageComboButton.h
@@ -0,0 +1,43 @@
+// Copyright (c) 2025, tsubasamusu All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/SCompoundWidget.h"
+
+class SComboButton;
+class SWidget;
+
+struct FLocalizedCulturesFlyweight
+{
+    TArray<FCultureRef> LocalizedCultures;
+
+    FLocalizedCulturesFlyweight()
+    {
+        constexpr bool bIncludeDerivedCultures = false;
+
+        const TArray<FString> LocalizedCultureNames = FTextLocalizationManager::Get().GetLocalizedCultureNames(ELocalizationLoadFlags::Editor);
+        
+        LocalizedCultures = FInternationalization::Get().GetAvailableCultures(LocalizedCultureNames, bIncludeDerivedCultures);
+    }
+};
+
+class SGptLanguageComboButton : public SCompoundWidget
+{
+	SLATE_BEGIN_ARGS(SGptLanguageComboButton) {}
+	SLATE_END_ARGS()
+
+public:
+	void Construct(const FArguments& InArgs, const TSharedRef<FLocalizedCulturesFlyweight>& InLocalizedCulturesFlyweight);
+
+private:
+	TSharedPtr<FLocalizedCulturesFlyweight> LocalizedCulturesFlyweight;
+
+	TSharedPtr<SComboButton> ComboButton;
+
+    FCulturePtr SelectedCulture;
+
+	FText GetDesiredComboButtonText() const;
+
+	TSharedRef<SWidget> OnGetComboButtonMenuContent();
+};
